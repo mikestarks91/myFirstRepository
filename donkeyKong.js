f2bad37420685c6780cb8bf6d748f4cc53afda62
mikestarks91 <mike.j.starks@gmail.com>
2015-03-30 16:03:59 -0600

    Simple js file

---
+++ donkeyKong.js
@@ -0,0 +1,10 @@
+var bananaNum = 78; //This is my comment
+while (bananaNum > 0) {
+	if (bananNum > 4) {
+		alert(bananaNum + "is plenty of bananas.");
+	}
+	else {
+		alert("I am disappointed in this banana pile.")
+	}
+	i--;
+}
